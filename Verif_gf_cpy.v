b9455e4a6f10169432d59a39e40745f3423901c1
david-hrnndz <102129342+david-hrnndz@users.noreply.github.com>
2022-07-18 16:31:09 -0400

    list_int_functions file created

--- Verif_gf_cpy.v
+++ Verif_gf_cpy.v
@@ -15,37 +15,6 @@ Definition t_gf := Tstruct __257 noattr.
 
 Local Open Scope Z.
 
-(* Function to split a [large] integer into a list representation *
- * of size number of limbs (16 in this case).                     *)
-Definition int_to_list (N : Z) : list Z := 
-    [    N mod 2^32;
-        (N/(2^32)) mod 2^32;
-        (N/(2^(32*2))) mod 2^32;
-        (N/(2^(32*3))) mod 2^32;
-        (N/(2^(32*4))) mod 2^32;
-        (N/(2^(32*5))) mod 2^32;
-        (N/(2^(32*6))) mod 2^32;
-        (N/(2^(32*7))) mod 2^32;
-        (N/(2^(32*8))) mod 2^32;
-        (N/(2^(32*9))) mod 2^32;
-        (N/(2^(32*10))) mod 2^32;
-        (N/(2^(32*11))) mod 2^32;
-        (N/(2^(32*12))) mod 2^32;
-        (N/(2^(32*13))) mod 2^32;
-        (N/(2^(32*14))) mod 2^32;
-        (N/(2^(32*15))) mod 2^32
-    ].
-
-About data_at.
-
-Fixpoint list_to_int' (l  : list Z) (n : Z) : Z := 
-    match l with 
-    | nil => 0
-    | h :: t => (h * (Z.pow 2 (32 * (n)))) + (list_to_int' t (n+1))
-    end. 
-
-Definition list_to_int (l : list Z) := list_to_int' l 0.
-
 (* Definition gf_cpy_spec : ident * funspec :=
     DECLARE _gf_cpy
     WITH 
---
+++ list_int_functions.v
@@ -0,0 +1,35 @@
+Require Import ZArith.
+Require Import stdpp.list.
+
+(*  Function to split a [large] integer into a list representation 
+    of size number of limbs (16 in this case).*)
+
+Local Open Scope Z. 
+ Definition int_to_list (N : Z) : list Z := 
+    [    N mod 2^32;
+        (N/(2^32)) mod 2^32;
+        (N/(2^(32*2))) mod 2^32;
+        (N/(2^(32*3))) mod 2^32;
+        (N/(2^(32*4))) mod 2^32;
+        (N/(2^(32*5))) mod 2^32;
+        (N/(2^(32*6))) mod 2^32;
+        (N/(2^(32*7))) mod 2^32;
+        (N/(2^(32*8))) mod 2^32;
+        (N/(2^(32*9))) mod 2^32;
+        (N/(2^(32*10))) mod 2^32;
+        (N/(2^(32*11))) mod 2^32;
+        (N/(2^(32*12))) mod 2^32;
+        (N/(2^(32*13))) mod 2^32;
+        (N/(2^(32*14))) mod 2^32;
+        (N/(2^(32*15))) mod 2^32
+    ].
+
+(*  Function (body) to unify the list representation of a large integer 
+    so that it is an integer. *)
+Fixpoint list_to_int' (l  : list Z) (n : Z) : Z := 
+    match l with 
+    | nil => 0
+    | h :: t => (h * (Z.pow 2 (32 * (n)))) + (list_to_int' t (n+1))
+    end. 
+
+Definition list_to_int (l : list Z) := list_to_int' l 0.
