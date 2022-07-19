c7710a188021b6f26b12b23f229e6897fa089b05
Jaziel Torres <jaziel.torres@upr.edu>
2022-07-19 15:27:20 -0400

    A lot of changes

--- Verif_gf_cpy.v
+++ Verif_gf_cpy.v
@@ -1,4 +1,5 @@
 Require Import VST.floyd.proofauto.
+Require Import VST.floyd.list_solver.
 Require Import x448.
 Require Import stdpp.list.
 Require Import ZArith.
@@ -14,31 +15,6 @@ Definition t_gf := Tstruct __257 noattr.
  list that represents the number N \in GF(prime) in 16 limbs. *)
 
 Local Open Scope Z.
-
-(* Definition gf_cpy_spec : ident * funspec :=
-    DECLARE _gf_cpy
-    WITH 
-        x: val,
-        shx : share,
-        contents_x : list val,
-        y: val,
-        shy : share,
-        contents_y : list int,
-        gv : globals
-    PRE [ tptr t_gf, tptr t_gf ]
-        PROP   (writable_share shx;
-                readable_share shy;
-                Zlength contents_x = 16;
-                Zlength contents_y = 16)
-        PARAMS (x ; y) GLOBALS (gv)
-        SEP    (field_at shx t_gf (DOT _limb) contents_x x;
-                field_at shy t_gf (DOT _limb) (map Vint contents_y) y)
-    POST [ tvoid ]
-        PROP   ()
-        RETURN ()
-        SEP    (field_at shx t_gf (DOT _limb) (map Vint contents_y) x;
-                field_at shy t_gf (DOT _limb) (map Vint contents_y) y). *)
-
 
 Definition gf_cpy_spec : ident * funspec :=
     DECLARE _gf_cpy
@@ -65,7 +41,8 @@ Definition gf_cpy_spec : ident * funspec :=
                 field_at shy t_gf (DOT _limb) (map Vint (map Int.repr contents_y)) y).
 
 
-
+(* Compute list.take 2 [1;2;3;4;5] = sublist.sublist 0 0 [1;2;3;4;5].
+Compute list.drop 2 [1;2;3;4;5] = sublist.sublist 2 (Zlength [1;2;3;4;5]) [1;2;3;4;5]. *)
 
 Definition gf_cpy_Inv shx shy x y contents_x contents_y := 
     (EX i : Z,
@@ -76,12 +53,58 @@ Definition gf_cpy_Inv shx shy x y contents_x contents_y :=
                 i >= 0)
         LOCAL   (temp _x x; temp _y y)
         SEP     (field_at shx t_gf (DOT _limb) (
-                    (list.take (Z.to_nat i) (map Vint (map Int.repr contents_y))) 
-                    ++ (list.drop (Z.to_nat i) contents_x)) x;
+                    (sublist.sublist 0 i (map Vint (map Int.repr contents_y))) 
+                    ++ (sublist.sublist i (Zlength contents_x) contents_x)) x;
                 field_at shy t_gf (DOT _limb) (map Vint (map Int.repr contents_y)) y)))%assert.
 
 
 Definition Gprog : funspecs := ltac:(with_library prog [ gf_cpy_spec ]).
+
+
+(* Lemma L1 (i : Z) (l : list val) :
+0 <= i < Zlength l ->
+upd_Znth i (take (Z.to_nat i) l) (Znth i l) =  take (Z.to_nat (i + 1)) l.
+Proof. Admitted.
+
+Lemma L2 (i : Z) (l1 l2 : list val) :
+0 <= i < Zlength l1 ->
+0 <= i < Zlength l2 ->
+upd_Znth i (take (Z.to_nat i) l1 ++ drop (Z.to_nat i) l2) (Znth i l1) 
+    =  take (Z.to_nat (i + 1)) l1 ++
+       drop (Z.to_nat (i + 1)) l2.
+Proof.
+    intros.
+    rewrite upd_Znth_unfold.
+    simpl.
+    assert (sublist.sublist 0 i (take (Z.to_nat i) l1 ++ drop (Z.to_nat i) l2) = take (Z.to_nat i) l1).
+    admit.
+    rewrite H1.
+    replace (Z.to_nat (i + 1)) with (S (Z.to_nat i)) by lia.
+    apply take_S_r. *)
+
+Lemma L1 (i : Z) (l1 l2 : list val) : 
+0 <= i < Zlength l1 ->
+sublist.sublist 0 i (sublist.sublist 0 i l1 ++ l2) = sublist.sublist 0 i l1.
+Proof. Admitted.
+
+Lemma L1' (i : Z) (l1 l2 : list val) : 
+0 <= i < Zlength l1 ->
+Zlength l1 = Zlength l2 ->
+sublist.sublist (i + 1) (Zlength l2) (sublist.sublist 0 i l1 ++
+    sublist.sublist i (Zlength l2) l2) = sublist.sublist (i + 1) (Zlength l2) l2.
+Proof. Admitted.
+
+Lemma L2 (i : Z) (l : list Z) : 
+0 <= i < Zlength l ->
+(sublist.sublist 0 i (map Vint (map Int.repr l))) ++
+[Vint (Int.repr (Znth i l))] = sublist.sublist 0 (i+1) (map Vint (map Int.repr l)).
+Proof. Admitted.
+
+Lemma L3 (i : Z) (l1 l2 : list val) :
+0 <= i < Zlength l1 ->
+Zlength l1 = Zlength l2 ->
+Zlength (sublist.sublist 0 i l1 ++ sublist.sublist i (Zlength l2) l2) = Zlength l2.
+Proof. Admitted.
 
 Lemma body_gf_cpy : semax_body Vprog Gprog f_gf_cpy gf_cpy_spec.
 Proof.
@@ -89,8 +112,53 @@ Proof.
     forward.
     forward_for_simple_bound 16 (gf_cpy_Inv shx shy x y contents_x contents_y).
     - entailer!.
+        admit.
     - try repeat forward.
         entailer!.
+        rewrite upd_Znth_unfold.
+        rewrite L1.
+        rewrite app_assoc.
+        rewrite L2.
+        rewrite L3.
+        rewrite L1'.
+        cancel.
+
+
+
+
+        try repeat rewrite Zlength_map.
+        by rewrite H0.
+        try repeat rewrite Zlength_map.
+        by rewrite H0.
+        try repeat rewrite Zlength_map; by rewrite H0.
+        try repeat rewrite Zlength_map; by rewrite H0.
+        by rewrite H0.
+        try repeat rewrite Zlength_map; by rewrite H0.
+        
+
+        About sublist.sublist.
+
+        list_simplify.
+
+    - entailer!.
+        rewrite <- H0.
+        assert (Zlength contents_y = Zlength (map Vint (map Int.repr contents_y))) by list_simplify.
+        rewrite H6 at 1.
+        assert (sublist.sublist 0 (Zlength (map Vint (map Int.repr contents_y)))
+        (map Vint (map Int.repr contents_y)) = (map Vint (map Int.repr contents_y))) by list_solve.
+        rewrite H7.
+        rewrite H, H0.
+        assert (sublist.sublist 16 16 contents_x = []) by list_solve.
+        rewrite H8.
+        rewrite app_nil_r.
+        cancel.
+        Search (Zlength )
+        list_simplify.
+        Compute sublist.sublist 0 2 [1;2;3;4;5] ++ [8;9].
+        About upd_Znth_unfold.
+        entailer!.
+        Check list.take_ge.
+        Search "take" inside stdpp.list.
         replace (Z.to_nat (i + 1)) with (S (Z.to_nat i)).
 Admitted.
 (* Qed. *)
